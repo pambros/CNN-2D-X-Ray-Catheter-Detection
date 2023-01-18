b79c802c0a8b0fc6b4ee28f56c85b9d347b45cdb
ArathyBastin <52795069+ArathyBastin@users.noreply.github.com>
2023-01-18 12:21:40 +0100

    Update globalVariables.default.bat

--- userSpecific/globalVariables.default.bat
+++ userSpecific/globalVariables.default.bat
@@ -1,17 +1,17 @@
 @set _UTIL_DIR=C:/Programs
 
-@rem @set _MINGW_BIN_DIR=%_UTIL_DIR%/mingw64/bin
+@rem @set _ARATH_BIN_DIR=%_UTIL_DIR%/arath64/bin
 
 @set _MSVC_DIR=%_UTIL_DIR%/Microsoft Visual Studio 14.0
 @set _MSVC_BIN_DIR=%_MSVC_DIR%/VC/bin
 @set _MSVC_BIN_X64_DIR=%_MSVC_BIN%/amd64
 @set _MSVC_BIN_X86_AMD64_DIR=%_MSVC_BIN_DIR%/x86_amd64
 
-@set _PYTHON_VERSION=3.7
+@set _PYTHON_VERSION=3.8.8
 @set _PYTHON_DIR=%_UTIL_DIR%/Python/
 @set _PYTHON_INCLUDE_DIR=%_PYTHON_DIR%/include
-@set _PYTHON_LIB=%_PYTHON_DIR%/libs/python37.lib
-@rem @set _PYTHON_LIB=%_PYTHON_DIR%/libs/libpython37.a
+@set _PYTHON_LIB=%_PYTHON_DIR%/libs/python38.lib
+@rem @set _PYTHON_LIB=%_PYTHON_DIR%/libs/libpython38.a
 @set _PYTHON="%_PYTHON_DIR%/python.exe"
 
 @set _CUDA_DIR=%_UTIL_DIR%/CUDA
@@ -21,20 +21,20 @@
 
 @rem name of the compiler using cmake syntax
 @set _COMPILER_NAME="Visual Studio 14 2015 Win64"
-@rem @set _COMPILER_NAME="MinGW Makefiles"
+@rem @set _COMPILER_NAME="AraBa Makefiles"
 
 @set _CPU=x64
 
 @set _LIBS_DIR=C:/Libs
 
 @set _BOOST_SRC_DIR=%_LIBS_DIR%/boost
-@set _BOOST_PYTHON_LIB=python37
-@set _BOOST_NUMPY_LIB=numpy37
+@set _BOOST_PYTHON_LIB=python38
+@set _BOOST_NUMPY_LIB=numpy38
 @rem @set _BOOST_LIB_DIR=%_LIBS_DIR%/boost_build/lib
 
 @set PATH= 
 @set PATH=%PATH%;C:/Windows/system32
-@rem @set PATH=%PATH%;%_MINGW_BIN_DIR%
+@rem @set PATH=%PATH%;%_ARATH_BIN_DIR%
 @set PATH=%PATH%;%_CUDA_DIR%/bin
 @set PATH=%PATH%;%_CUDA_DIR%/libnwp
 @set PATH=%PATH%;%_MSVC_BIN_X64_DIR%
